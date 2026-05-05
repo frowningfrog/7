26962c08c98cc8423c4ed6005bb827d74775a3c2
frowningfrog <natalizedstolpinator@hotmail.com>
2026-05-05 08:19:04 -0600

    init

---
+++ fruit.js
@@ -0,0 +1,12 @@
+// take in color function
+
+// check the file for that fruit
+// if there, print its color
+// if not, ask for its color
+
+// then take in color function again
+const net = require("net");
+const fs = require("fs");
+
+const log_file = "server.log";
+const clients = new Map();
